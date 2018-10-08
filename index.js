184c900a93c768a8bd7f58c0dbc0afe656fbf81a
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 16:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,3 +58,11 @@ function handlebarsSetup() {
 }
 
 
+function init() {
+  //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  initForm()
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  init()
+})
